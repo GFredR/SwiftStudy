e93c86232711bdde9a81eff628a452d75b0dee7c
GFredR <664626586@qq.com>
2021-03-08 16:25:21 +0800

    A little...

---
+++ NetEaseCloudMusic/NetEaseCloudMusic/Discovery(发现)/Recomend(推荐)/Models/RecomendModel.swift
@@ -0,0 +1,8 @@
+//
+//  RecomendModel.swift
+//  NetEaseCloudMusic
+//
+//  Created by 郭丰锐 on 2021/3/8.
+//
+
+import Foundation
---
+++ Spotify/Spotify/Models/AuthResponse.swift
@@ -0,0 +1,8 @@
+//
+//  AuthResponse.swift
+//  Spotify
+//
+//  Created by 郭丰锐 on 2021/3/8.
+//
+
+import Foundation
